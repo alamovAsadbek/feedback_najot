482c196a94be73fc4d0e32b5371e7b981ef2667a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-29 16:44:02 +0500

    change script file

--- main/forms/offer/script.js
+++ main/forms/offer/script.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const signInBtn = document.getElementById('signInBtn');
+    const signUpBtn = document.getElementById('signUpBtn');
+    const signInForm = document.getElementById('signInForm');
+    const signUpForm = document.getElementById('signUpForm');
+
+    signInBtn.addEventListener('click', function () {
+        signInForm.style.display = 'block';
+        signUpForm.style.display = 'none';
+    });
+
+    signUpBtn.addEventListener('click', function () {
+        signInForm.style.display = 'none';
+        signUpForm.style.display = 'block';
+    });
+
     const form = document.querySelector('form');
     form.addEventListener('submit', function (event) {
         event.preventDefault();
